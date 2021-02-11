eb46c082879d8a4e7c034117aefab07e52a43336
Rackarn <22744125+MichaelEngdahl@users.noreply.github.com>
2021-02-11 13:50:08 +0100

    header and menu ready

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,25 @@
-console.log("HELLO")
+const btnHamburger = document.querySelector("#btnHamburger")
+const header = document.querySelector(".header")
+const overlay = document.querySelector(".overlay")
+const fadeElems = document.querySelectorAll(".has-fade")
+const body = document.querySelector("body")
+
+btnHamburger.addEventListener('click', () => {
+  console.log("click hamburger")
+
+  if(header.classList.contains('open')) { // Close burger menu
+    body.classList.remove('noScroll')
+    header.classList.remove('open')
+    fadeElems.forEach((element) => {
+      element.classList.remove('fade-in')
+      element.classList.add('fade-out')
+    })
+  } else { // Open burger menu
+    body.classList.add('noScroll')
+    header.classList.add('open')
+    fadeElems.forEach(function(element) {
+      element.classList.remove('fade-out')
+      element.classList.add('fade-in')
+    })
+  }
+})
